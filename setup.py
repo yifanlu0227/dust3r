4585e46b6b50d3341cad83fe67473e3e4a380e87
Yifan Lu <yiflu@nvidia.com>
2024-08-22 05:57:53 -0700

    [yfl] add croco sub packages

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='DUST3R',
     version='0.1.0',
     # packages=find_packages(),
-    packages=['dust3r', 'dust3r.utils', 'dust3r.heads', 'dust3r.datasets', 'dust3r.cloud_opt', 'croco'],
+    packages=['dust3r', 'dust3r.utils', 'dust3r.heads', 'dust3r.datasets', 'dust3r.cloud_opt', 
+              'croco', 'croco.models', 'croco.stereoflow', 'croco.utils'],
     author='naver',
 )
