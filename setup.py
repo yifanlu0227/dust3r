68e636f34a4a753a960b3e268bdb3ed5a1ddcffb
Yifan Lu <yiflu@nvidia.com>
2024-08-22 05:50:54 -0700

    [yfl] add more package

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='DUST3R',
     version='0.1.0',
     # packages=find_packages(),
-    packages=['dust3r'],
+    packages=['dust3r', 'dust3r.utils', 'dust3r.heads', 'dust3r.datasets', 'dust3r.cloud_opt'],
     author='naver',
 )
